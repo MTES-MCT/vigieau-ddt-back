2dd317ea2eaf2a44a6b680cbb03e7cde0319e675
Vincent Lainé <vincent.laine.utc@gmail.com>
2024-06-05 14:37:06 +0200

    feedback usages ✨

--- src/usage_feedback/usage_feedback.controller.ts
+++ src/usage_feedback/usage_feedback.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsageFeedbackService } from './usage_feedback.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UsageFeedback } from './entities/usage_feedback.entity';
 
-@Controller('usage-feedback')
+@Controller('usage_feedback')
 export class UsageFeedbackController {
-  constructor(private readonly usageFeedbackService: UsageFeedbackService) {}
+  constructor(private readonly usageFeedbackService: UsageFeedbackService) {
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'Retourne tout les feedbacks suivant le rôle de l\'utilisateur',
+  })
+  @ApiResponse({
+    status: 201,
+    type: [UsageFeedback],
+  })
+  async findAll(@Req() req): Promise<UsageFeedback[]> {
+    return this.usageFeedbackService.findAll(req.session.user)
+  }
 }
--- src/usage_feedback/usage_feedback.service.ts
+++ src/usage_feedback/usage_feedback.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsageFeedback } from './entities/usage_feedback.entity';
 
 @Injectable()
-export class UsageFeedbackService {}
+export class UsageFeedbackService {
+  constructor(@InjectRepository(UsageFeedback)
+              private readonly usageFeedbackRepository: Repository<UsageFeedback>) {
+  }
+
+  findAll(currentUser: any): any {
+    const whereClause = {
+      isView: false,
+    };
+    if (currentUser && currentUser.role !== 'mte') {
+      whereClause['usage'] = {
+        restriction: {
+          arreteRestriction: {
+            departement: {
+              code: currentUser.role_departement,
+            },
+          },
+        },
+      };
+    }
+
+    return this.usageFeedbackRepository.find({
+      select: {
+        id: true,
+        createdAt: true,
+        feedback: true,
+        usage: {
+          id: true,
+          nom: true,
+          thematique: {
+            id: true,
+            nom: true,
+          },
+          restriction: {
+            id: true,
+            arreteRestriction: {
+              id: true,
+              departement: {
+                code: true,
+              },
+            },
+          },
+        },
+      },
+      relations: ['usage', 'usage.thematique', 'usage.restriction', 'usage.restriction.arreteRestriction', 'usage.restriction.arreteRestriction.departement'],
+      where: whereClause,
+    });
+  }
+}
